14b76856312beeeee18ee584bab35adfced98b97
Shashank Kumar <shsharma93@gmail.com>
2020-02-09 19:20:16 +1000

    build folder added for deployment

---
+++ build/precache-manifest.87c49966992d66baca01efe773ff1d22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c84cfe14da8a0198eab6b4ed6cc726f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28125598e59eace5e5e2",
+    "url": "/static/css/main.42643ea7.chunk.css"
+  },
+  {
+    "revision": "728191344766c890e74d",
+    "url": "/static/js/2.3d7d4dd2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.3d7d4dd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28125598e59eace5e5e2",
+    "url": "/static/js/main.24e32e46.chunk.js"
+  },
+  {
+    "revision": "a0e2fe78072e78a55c16",
+    "url": "/static/js/runtime-main.d971a05f.js"
+  }
+]);
